3906791cde3c54b95cca236e906c4027bbe5ee57
bertmad3400 <skrivtilbertram@gmail.com>
2021-12-15 19:27:41 +0100

    Add python file for setup

---
+++ setup.py
@@ -0,0 +1,34 @@
+import sqlite3
+
+import os
+
+from mainFlask import DBName
+
+def setupDB():
+    if os.path.exists(DBName):
+        print(f"Can't continue, please remove DB file by name {DBName} first")
+        exit()
+
+    conn = sqlite3.connect(DBName)
+    cur = conn.cursor()
+
+    cur.execute('''CREATE TABLE posts
+               (    id integer PRIMARY KEY AUTOINCREMENT NOT NULL,
+                    title text NOT NULL,
+                    author text NOT NULL,
+                    content text,
+                    image_id text,
+                    inserted_at datetime DEFAULT CURRENT_TIMESTAMP)''')
+
+    cur.execute('''CREATE TABLE comments
+               (    id integer PRIMARY KEY AUTOINCREMENT NOT NULL,
+                    post_id integer NOT NULL,
+                    author text NOT NULL,
+                    content text NOT NULL,
+                    inserted_at datetime DEFAULT CURRENT_TIMESTAMP)''')
+
+    conn.commit()
+    conn.close()
+
+if __name__ == "__main__":
+    setupDB()
